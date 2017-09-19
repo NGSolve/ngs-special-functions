b848ead47151dfaa11bf3a6a167a7456f2a97d4e
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2017-09-19 09:24:50 +0200

    Further simplify python export

---
+++ inc/specialcf.hpp
@@ -0,0 +1,81 @@
+#ifndef SPECIALCF_HPP_INCLUDED___
+#define SPECIALCF_HPP_INCLUDED___
+#include <fem.hpp>
+#include <tuple>
+
+namespace f2c {
+#include <f2c.h>
+}
+
+// undef defines in f2c.h
+#undef min
+#undef max
+#undef abs
+#include <python_ngstd.hpp>
+
+namespace ngfem {
+    template <typename T> class T_SpecialCoefficientFunction;
+
+    template <typename TRes, typename TFirstArg, typename ... TArgs> 
+    class T_SpecialCoefficientFunction<TRes(*)(TFirstArg, TArgs...) > : public CoefficientFunction
+    {
+        static constexpr bool is_complex = std::is_same<Complex, TRes>::value;
+        static constexpr bool is_double = std::is_same<double, TRes>::value;
+        static_assert(is_complex || is_double, "Result has to be either Complex or double");
+
+        typedef TRes (*TFunc)(TFirstArg, TArgs...);
+
+        TFunc func;
+        shared_ptr<CoefficientFunction> arg;
+        std::tuple<TFirstArg, TArgs...> parameters;
+
+      public:
+        T_SpecialCoefficientFunction( shared_ptr<CoefficientFunction> arg_, TFunc func_, TArgs ... args )
+          : CoefficientFunction(1, is_complex),
+            func(func_), arg(arg_), parameters({}, args...)
+        {
+        }
+
+        double Evaluate (const BaseMappedIntegrationPoint & ip) const override 
+        {
+            if constexpr(is_complex)
+              throw Exception("T_SpecialCoefficientFunction::Evaluate called for complex function");
+            else {
+                double z = arg->Evaluate(ip);
+                auto arguments = parameters;
+                std::get<0>(arguments) = z;
+                return std::apply(func, arguments);
+            }
+        }
+
+        virtual void Evaluate(const BaseMappedIntegrationPoint & ip, FlatVector<Complex> result) const override
+        {
+            if constexpr(is_complex)
+            {
+                Complex z = arg->EvaluateComplex(ip);
+                auto arguments = parameters;
+                std::get<0>(arguments) = z;
+                result(0) = std::apply(func, arguments);
+            }
+            
+        }
+
+        template<typename ...TPyArgs>
+        static void ExportPython( py::module &m, string name, TFunc func, TPyArgs ... py_args )
+        {
+            m.def(name.c_str(), [func] (shared_ptr<ngfem::CoefficientFunction> arg, TArgs ... args) -> shared_ptr<ngfem::CoefficientFunction>
+                  {
+                  return make_shared<ngfem::T_SpecialCoefficientFunction<TRes(*)(TFirstArg, TArgs...) >>(arg, func, args...);
+                  },  py_args...
+            );
+        }
+    };
+
+}
+
+
+template<typename TFunc, typename ...TPyArgs>
+static void ExportPythonSpecialCF( py::module &m, string name, TFunc func, TPyArgs ... py_args ) {
+    ngfem::T_SpecialCoefficientFunction<TFunc>::ExportPython(m, name, func, py_args...);
+}
+#endif // SPECIALCF_HPP_INCLUDED___
--- src/specialcf.cpp
+++ src/specialcf.cpp
@@ -1,10 +1,4 @@
-#include <fem.hpp>
-#include <tuple>
-#include <experimental/tuple>
-
-namespace f2c {
-#include <f2c.h>
-}
+#include <specialcf.hpp>
 
 extern "C" /* Subroutine */ int zbesi_(f2c::doublereal *zr, f2c::doublereal *zi, const f2c::doublereal *fnu, const f2c::integer *kode, const f2c::integer *n, f2c::doublereal *cyr, f2c::doublereal *cyi, f2c::integer * nz, f2c::integer *ierr);
 extern "C" /* Subroutine */ f2c::doublereal gamln_(f2c::real *z__, f2c::integer *ierr);
@@ -39,128 +33,16 @@ double gamln(double x_)
   return res;
 }
 
-template<typename ...Args> struct FuncTraits{};
-
-template<typename TRes, typename TFirstArg, typename ...TArgs>
-struct FuncTraits<TRes(TFirstArg, TArgs...)>
-{
-  typedef TRes res_type;
-  typedef TFirstArg first_arg_type;
-  typedef std::tuple<TArgs...> args_tuple_type;
-
-};
-
-namespace ngfem {
-    template<typename TRes, typename TFirstArg, typename ...TArgs>
-    class T_SpecialCoefficientFunction : public CoefficientFunction
-    {
-        static constexpr bool is_complex = std::is_same<Complex, TRes>::value;
-        static constexpr bool is_double = std::is_same<double, TRes>::value;
-        static_assert(is_complex || is_double, "Result has to be either Complex or double");
-
-        typedef TRes (*TFunc)(TFirstArg, TArgs...);
-
-        TFunc func;
-        shared_ptr<CoefficientFunction> arg;
-        std::tuple<TFirstArg, TArgs...> parameters;
-
-      public:
-        T_SpecialCoefficientFunction( shared_ptr<CoefficientFunction> arg_, TFunc func_, TArgs ... args )
-          : CoefficientFunction(1, is_complex),
-            func(func_), arg(arg_), parameters({}, args...)
-        {
-        }
-
-        double Evaluate (const BaseMappedIntegrationPoint & ip) const override 
-        {
-            if constexpr(is_complex)
-              throw Exception("T_SpecialCoefficientFunction::Evaluate called for complex function");
-            else {
-                double z = arg->Evaluate(ip);
-                auto arguments = parameters;
-                std::get<0>(arguments) = z;
-                return std::experimental::apply(func, arguments);
-            }
-        }
-
-        virtual void Evaluate(const BaseMappedIntegrationPoint & ip, FlatVector<Complex> result) const override
-        {
-            if constexpr(is_complex)
-            {
-                Complex z = arg->EvaluateComplex(ip);
-                auto arguments = parameters;
-                std::get<0>(arguments) = z;
-                result(0) = std::experimental::apply(func, arguments);
-            }
-            
-        }
-
-    };
-
-    class SpecialCoefficientFunction_ZBESI : public CoefficientFunction
-    {
-        shared_ptr<CoefficientFunction> arg;
-
-        double order;
-        int kode;
-
-      public:
-
-        SpecialCoefficientFunction_ZBESI(shared_ptr<CoefficientFunction> arg_, double order_, int kode_)
-          : CoefficientFunction(1, true),
-          arg(arg_), order(order_), kode(kode_)
-        {
-        }
-
-        double Evaluate (const BaseMappedIntegrationPoint & ip) const override 
-        {
-            throw Exception("SpecialCoefficientFunction_ZBESI::Evaluate called");
-        }
-
-
-        virtual void Evaluate(const BaseMappedIntegrationPoint & ip, FlatVector<Complex> result) const override
-        {
-            Complex z = arg->EvaluateComplex(ip);
-            result(0) = zbesi(z, order, kode);
-        }
-    };
-
-
-
-
-    class SpecialCoefficientFunction_DGAMLN : public CoefficientFunction
-    {
-        shared_ptr<CoefficientFunction> arg;
-
-      public:
-        SpecialCoefficientFunction_DGAMLN(shared_ptr<CoefficientFunction> arg_)
-          : CoefficientFunction(1, false),
-          arg(arg_)
-        { }
-
-        virtual double Evaluate (const BaseMappedIntegrationPoint & ip) const override {
-            double x = arg->Evaluate(ip);
-            return gamln(x);
-        }
-
-    };
-}
-
 #undef min
 #undef max
 #undef abs
 #include <python_ngstd.hpp>
 PYBIND11_MODULE(special_functions, m) {
-    m.def("Gamma", [] (shared_ptr<ngfem::CoefficientFunction> arg) -> shared_ptr<ngfem::CoefficientFunction>
-          {
-          return make_shared<ngfem::T_SpecialCoefficientFunction<double, double>>(arg, gamln);
-          });
+    ExportPythonSpecialCF(m, "Gamma", gamln);
 
-    m.def("Bessel", [] (shared_ptr<ngfem::CoefficientFunction> arg, double order, int kode) -> shared_ptr<ngfem::CoefficientFunction>
-          {
-          return make_shared<ngfem::T_SpecialCoefficientFunction<Complex, Complex, double, int>>(arg, zbesi, order, kode);
-          }, py::arg("z"), py::arg("order")=0, py::arg("kode")=1, py::doc(R"DOCSTRING_(
-
+    ExportPythonSpecialCF(m, "Bessel", zbesi,
+           py::arg("z"), py::arg("order")=0, py::arg("kode")=1, py::doc(R"DOCSTRING_(
+Complex Bessel functions
 Input
   z      - Complex argument
   order  - DOUBLE PRECISION order>=0
@@ -170,6 +52,5 @@ Input
                =2  returns
                    CY=exp(-abs(X))*I(order,z)
                    where X=Re(z)
-)DOCSTRING_")
-    );
+)DOCSTRING_"));
 }
